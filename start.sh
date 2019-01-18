430b42242809894d4dc9433b4242d88787f59741
John Salatas <jsalatas@gmail.com>
2019-01-18 00:36:33 -0800

    Dell XPS 15 finalize adaptation

--- start.sh
+++ start.sh
@@ -2,11 +2,20 @@
 cd ~/.conky
 killall conky
 conky -c clock/conkyrc &
+sleep 1
 conky -c date/conkyrc &
+sleep 1
 conky -c graphs/conkyrc_cpu &
+sleep 1
 conky -c graphs/conkyrc_network &
+sleep 1
 conky -c graphs/conkyrc_memory &
+sleep 1
 conky -c disk/conkyrc &
+sleep 1
 conky -c sensors/conkyrc &
-#conky -c processes/conkyrc_cpu &
-#conky -c processes/conkyrc_memory &
+sleep 1
+conky -c processes/conkyrc_cpu &
+sleep 1
+conky -c processes/conkyrc_memory &
+sleep 1
