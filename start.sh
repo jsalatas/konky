6a28075ec5123971c2b0d3b78f925574ebb7d47f
John Salatas <jsalatas@gmail.com>
2019-01-27 12:37:33 -0800

    demonize conky

--- start.sh
+++ start.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 cd ~/.conky
 killall conky
-conky -c clock/conkyrc &
-sleep 1
-conky -c date/conkyrc &
-sleep 1
-conky -c graphs/conkyrc_cpu &
-sleep 1
-conky -c graphs/conkyrc_network &
-sleep 1
-conky -c graphs/conkyrc_memory &
-sleep 1
-conky -c disk/conkyrc &
-sleep 1
-conky -c sensors/conkyrc &
-sleep 1
-conky -c processes/conkyrc_cpu &
-sleep 1
-conky -c processes/conkyrc_memory &
-sleep 1
+conky -dqc clock/conkyrc
+conky -dqc date/conkyrc
+conky -dqc graphs/conkyrc_cpu
+conky -dqc graphs/conkyrc_network
+conky -dqc graphs/conkyrc_memory
+conky -dqc disk/conkyrc
+conky -dqc sensors/conkyrc
+conky -dqc processes/conkyrc_cpu
+conky -dqc processes/conkyrc_memory
